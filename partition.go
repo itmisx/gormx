df64122d9727d48659b54b0622df074b9868c16a
itmisx <itmisx@126.com>
2025-05-18 14:33:57 +0800

    ✨ 自动分区管理

--- partition.go
+++ partition.go
@@ -17,12 +17,15 @@ package gormx
 //   PARTITION p20250601 VALUES LESS THAN (UNIX_TIMESTAMP('2025-06-01'))
 //   PARTITION pmax VALUES LESS THAN MAXVALUE  // !!! 如果需要动态新增分区，该语句需要去掉，否则会报错，这是个兜底，不能再加分区
 // );
+//
+// &partition.Start() 会自动启动一个协程，定期自动创建分区和移除过期分区
 
 import (
 	"context"
 	"fmt"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/dromara/carbon/v2"
 	"github.com/itmisx/logx"
@@ -31,119 +34,225 @@ import (
 )
 
 type partition struct {
-	db       *gorm.DB
-	database string // 数据库名
-	table    string // 表名
+	DB              *gorm.DB       // *gorm.DB
+	Database        string         // 数据库名
+	Table           string         // 表名
+	PartitionUnit   PartitionUnitT // 分区单位 1-按天 2-按月
+	RetentionMonths int            // 分区数据的保留时长
 }
+
+type PartitionUnitT int // 分区单元
+
+const (
+	PartitionUnitDay   PartitionUnitT = iota + 1 // 按天分区
+	PartitionUnitMonth                           // 按月分区
+	PartitionUnitYear                            // 按年分区
+)
+
+// 默认分区自动检查时间
+var DefaultCronDuration = time.Hour
 
 func NewPartition(
 	db *gorm.DB,
 	database string,
 	table string,
+	partitionUnit PartitionUnitT,
+	retentionMonths int,
 ) *partition {
 	return &partition{
-		db:       db,
-		database: database,
-		table:    table,
+		DB:              db,
+		Database:        database,
+		Table:           table,
+		PartitionUnit:   partitionUnit,
+		RetentionMonths: retentionMonths,
 	}
 }
 
 // List 获取所有分区
-func (p *partition) List(ctx context.Context) (partitions []string, err error) {
+func (p *partition) list(ctx context.Context) (partitions []string, err error) {
 	// 获取所有分区
-	err = p.db.Table("information_schema.PARTITIONS").
-		Where("TABLE_SCHEMA = ?", p.database).
-		Where("TABLE_NAME = ?", p.table).
+	err = p.DB.WithContext(ctx).Table("information_schema.PARTITIONS").
+		Where("TABLE_SCHEMA = ?", p.Database).
+		Where("TABLE_NAME = ?", p.Table).
 		Where("PARTITION_NAME IS NOT NULL").
 		Pluck("PARTITION_NAME", &partitions).
 		Error
 	return partitions, err
 }
 
-// ExistsCurrentMonth 是否存在当月分区
-func (p *partition) ExistsCurrentMonthPartition(ctx context.Context) (bool, error) {
-	partitions, err := p.List(ctx)
+// ExistsDayPartition 是否存在日分区
+func (p *partition) existsDayPartition(ctx context.Context, days int) (bool, error) {
+	partitions, err := p.list(ctx)
 	if err != nil {
 		return false, err
 	}
-	partitionName := "p" + strings.ReplaceAll(carbon.Now().AddMonths(1).StartOfMonth().ToDateString(), "-", "")
+	partitionName := "p" + strings.ReplaceAll(carbon.Now().AddDays(days).StartOfDay().ToDateString(), "-", "")
 	return lo.Contains(partitions, partitionName), nil
 }
 
-// ExistsNextMonth 是否存在下个月分区
-func (p *partition) ExistsNextMonthPartition(ctx context.Context) (bool, error) {
-	partitions, err := p.List(ctx)
+// ExistsMonthPartition 是否存在月分区
+func (p *partition) existsMonthPartition(ctx context.Context, months int) (bool, error) {
+	partitions, err := p.list(ctx)
 	if err != nil {
 		return false, err
 	}
-	partitionName := "p" + strings.ReplaceAll(carbon.Now().AddMonths(2).StartOfMonth().ToDateString(), "-", "")
+	partitionName := "p" + strings.ReplaceAll(carbon.Now().AddMonths(months).StartOfMonth().ToDateString(), "-", "")
 	return lo.Contains(partitions, partitionName), nil
 }
 
-// AddCurrentMonthPartition 新增当月分区
-func (p *partition) AddCurrentMonthPartition(ctx context.Context) error {
-	exists, err := p.ExistsCurrentMonthPartition(ctx)
+// ExistsYearPartition 是否存在年分区
+func (p *partition) existsYearPartition(ctx context.Context, years int) (bool, error) {
+	partitions, err := p.list(ctx)
+	if err != nil {
+		return false, err
+	}
+	partitionName := "p" + strings.ReplaceAll(carbon.Now().AddYears(years).StartOfYear().ToDateString(), "-", "")
+	return lo.Contains(partitions, partitionName), nil
+}
+
+// AddDayPartition 新增日分区
+func (p *partition) addDayPartition(ctx context.Context, days int) error {
+	exists, err := p.existsDayPartition(ctx, days)
 	if err != nil {
 		return err
 	}
 	if exists {
 		return nil
 	}
-	partitionName := "p" + strings.ReplaceAll(carbon.Now().AddMonths(1).StartOfMonth().ToDateString(), "-", "")
-	partitionRange := carbon.Now().AddMonths(1).StartOfMonth().ToDateString()
+	partitionName := "p" + strings.ReplaceAll(carbon.Now().AddDays(days).StartOfDay().ToDateString(), "-", "")
+	partitionRange := carbon.Now().AddDays(days).StartOfDay().ToDateString()
 	createPartitionSQL := fmt.Sprintf(
 		"ALTER TABLE %s ADD PARTITION(PARTITION %s VALUES LESS THAN (UNIX_TIMESTAMP('%s')))",
-		p.table,
+		p.Table,
 		partitionName,
 		partitionRange,
 	)
-	return p.db.Exec(createPartitionSQL).Error
+	return p.DB.WithContext(ctx).Exec(createPartitionSQL).Error
 }
 
-// AddNextMonthPartition 新增下月分区
-func (p *partition) AddNextMonthPartition(ctx context.Context) error {
-	exists, err := p.ExistsNextMonthPartition(ctx)
+// AddMonthPartition 新增月分区
+func (p *partition) addMonthPartition(ctx context.Context, months int) error {
+	exists, err := p.existsMonthPartition(ctx, months)
 	if err != nil {
 		return err
 	}
 	if exists {
 		return nil
 	}
-	partitionName := "p" + strings.ReplaceAll(carbon.Now().AddMonths(2).StartOfMonth().ToDateString(), "-", "")
-	partitionRange := carbon.Now().AddMonths(2).StartOfMonth().ToDateString()
-	sql := fmt.Sprintf(
+	partitionName := "p" + strings.ReplaceAll(carbon.Now().AddMonths(months).StartOfMonth().ToDateString(), "-", "")
+	partitionRange := carbon.Now().AddMonths(months).StartOfMonth().ToDateString()
+	createPartitionSQL := fmt.Sprintf(
 		"ALTER TABLE %s ADD PARTITION(PARTITION %s VALUES LESS THAN (UNIX_TIMESTAMP('%s')))",
-		p.table,
+		p.Table,
 		partitionName,
 		partitionRange,
 	)
-	return p.db.Exec(sql).Error
+	return p.DB.WithContext(ctx).Exec(createPartitionSQL).Error
 }
 
-// DropExpiredPartitions 删除过期分区
-func (p *partition) DropExpiredPartitions(ctx context.Context, expiredMonths int) (err error) {
-	if expiredMonths <= 0 {
+// AddYearPartition 新增年分区
+func (p *partition) addYearPartition(ctx context.Context, years int) error {
+	exists, err := p.existsYearPartition(ctx, years)
+	if err != nil {
+		return err
+	}
+	if exists {
 		return nil
 	}
-	partitions, err := p.List(ctx)
+	partitionName := "p" + strings.ReplaceAll(carbon.Now().AddYears(years).StartOfYear().ToDateString(), "-", "")
+	partitionRange := carbon.Now().AddYears(years).StartOfYear().ToDateString()
+	sql := fmt.Sprintf(
+		"ALTER TABLE %s ADD PARTITION(PARTITION %s VALUES LESS THAN (UNIX_TIMESTAMP('%s')))",
+		p.Table,
+		partitionName,
+		partitionRange,
+	)
+	return p.DB.WithContext(ctx).Exec(sql).Error
+}
+
+// dropExpiredPartitions 删除过期分区
+func (p *partition) dropExpiredPartitions(ctx context.Context) (err error) {
+	if p.RetentionMonths <= 0 {
+		return nil
+	}
+	partitions, err := p.list(ctx)
 	if err != nil {
 		return err
 	}
 	// 删除过期的分区
-	earliestPartition, _ := strconv.Atoi(strings.ReplaceAll(carbon.Now().SubMonths(expiredMonths-1).StartOfMonth().ToDateString(), "-", ""))
+	earliestPartition, _ := strconv.Atoi(strings.ReplaceAll(carbon.Now().SubMonths(p.RetentionMonths-1).StartOfMonth().ToDateString(), "-", ""))
 	for _, partition := range partitions {
 		partitionNum, _ := strconv.Atoi(strings.TrimLeft(partition, "p"))
 		if partitionNum < earliestPartition {
 			sql := fmt.Sprintf(
 				"ALTER TABLE %s DROP PARTITION p%d",
-				p.table,
+				p.Table,
 				partitionNum,
 			)
-			err = p.db.Exec(sql).Error
+			err = p.DB.WithContext(ctx).Exec(sql).Error
 			if err != nil {
-				logx.Error(context.Background(), fmt.Sprintf("drop  table %s partition %s failed", p.table, partition))
+				logx.Error(context.Background(), fmt.Sprintf("drop  table %s partition %s failed", p.Table, partition))
 			}
 		}
 	}
 	return err
+}
+
+// Start 启动分区自动管理
+// 创建3个避免数据找不到分区，分区语句按照utc转换的时间戳，这个时间戳和当前时间戳可能不一致
+func (p *partition) Start() {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
+	defer cancel()
+	// 初始化
+	switch p.PartitionUnit {
+	// 按天分区
+	case PartitionUnitDay:
+		p.addDayPartition(ctx, 0)
+		p.addDayPartition(ctx, 1)
+		p.addDayPartition(ctx, 2)
+	// 按月分区
+	case PartitionUnitMonth:
+		p.addMonthPartition(ctx, 0)
+		p.addMonthPartition(ctx, 1)
+		p.addMonthPartition(ctx, 2)
+	// 按年分区
+	case PartitionUnitYear:
+		p.addYearPartition(ctx, 0)
+		p.addYearPartition(ctx, 1)
+		p.addYearPartition(ctx, 2)
+	default:
+		panic("unsupported partition unit type")
+	}
+	// 定时检查，并自动创建分区，并删除过期的分区
+	go func() {
+		if DefaultCronDuration < time.Second*10 {
+			DefaultCronDuration = time.Second * 10
+		}
+		ticker := time.NewTicker(DefaultCronDuration)
+		for {
+			<-ticker.C
+			func() {
+				ctx1, cancel1 := context.WithTimeout(context.Background(), time.Second*30)
+				defer cancel1()
+				switch p.PartitionUnit {
+				// 按天分区
+				case PartitionUnitDay:
+					p.addDayPartition(ctx1, 0)
+					p.addDayPartition(ctx1, 1)
+					p.addDayPartition(ctx1, 2)
+				// 按月分区
+				case PartitionUnitMonth:
+					p.addMonthPartition(ctx1, 0)
+					p.addMonthPartition(ctx1, 1)
+					p.addMonthPartition(ctx1, 2)
+				// 按年分区
+				case PartitionUnitYear:
+					p.addYearPartition(ctx1, 0)
+					p.addYearPartition(ctx1, 1)
+					p.addYearPartition(ctx1, 2)
+				}
+				p.dropExpiredPartitions(ctx1)
+			}()
+		}
+	}()
 }
